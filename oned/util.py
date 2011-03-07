1b83daeaaf17cd003ca9096d2545f2dfc2683711
Andy Casey <acasey@mso.anu.edu.au>
2011-03-07 22:39:20 +1100

    added capability to take a list of lines instead of a single line

--- oned/util.py
+++ oned/util.py
@@ -1,9 +1,9 @@
-import onedspec
+from onedspec import onedspec
 import numpy as np
 import scipy
 import scipy.optimize
 
-def fitprof(spectrum, line, peak_pm=1.5):
+def fitprof(spectrum, lines, peak_pm=1.5):
     """
     
     Fits a profile to a given spectrum at the line point. The peak value must be
@@ -37,39 +37,58 @@ def fitprof(spectrum, line, peak_pm=1.5):
 
     if not isinstance(spectrum, onedspec):
         return ValueError('Spectrum must be a onedspec class object')
+    
         
     try:
-        line = float(line)
+        if isinstance(lines, list):
+            lines = [map(float, line) for line in lines]
+            
+        else:
+            lines = [float(lines)]
         
     except TypeError:
         raise TypeError('Line profile position must be a floating point.')
         
+    try:
+        peak_pm = float(peak_pm)
+    
+    except TypeError:
+        raise TypeError('Position error must be a floating point.')
         
     
-    peak_spectrum = spectrum[line - peak_pm:peak_pm + line]
+    profiles = []
     
-    peak = max(peak_spectrum.flux)
-    position = peak_spectrum.wavelength[peak_spectrum.flux.searchsorted(peak)]
-    
-    index = spectrum.wavelength.searchsorted(position)
-    
-    
-    p, m = 0, 0
-    num = len(spectrum.flux)
-    
-    while (num > index + p + 1) and (spectrum.flux[index + p] > spectrum.flux[index + p + 1]):
-        p += 1
-        
-    while (index - m - 1 > 0) and (spectrum.flux[index - m] > spectrum.flux[index - m - 1]):
-        m += 1
+    for line in lines:
 
-    fitfunc = lambda p, x: p[0] * scipy.exp(-(x - p[1])**2 / (2.0 * p[2]**2))
-    errfunc = lambda p, x, y: fitfunc(p, x) - y
+        peak_spectrum = spectrum[line - peak_pm:peak_pm + line]
+        
+        peak = max(peak_spectrum.flux)
+        index = scipy.where(peak_spectrum.flux==peak)[0][0]
+        position = peak_spectrum.wavelength[index]
+        
+        index = spectrum.wavelength.searchsorted(position)
+        
+        
+        p, m = 0, 0
+        num = len(spectrum.flux)
+        
+        while (num > index + p + 1) and (spectrum.flux[index + p] > spectrum.flux[index + p + 1]):
+            p += 1
+            
+        while (index - m - 1 > 0) and (spectrum.flux[index - m] > spectrum.flux[index - m - 1]):
+            m += 1
     
-    p0 = scipy.c_[peak, position, 5]
-    p1, success = scipy.optimize.leastsq(errfunc, p0.copy()[0], args=(spectrum[index - m:p + index].wavelength, spectrum[index - m:p + index].flux))
+        fitfunc = lambda p, x: p[0] * scipy.exp(-(x - p[1])**2 / (2.0 * p[2]**2))
+        errfunc = lambda p, x, y: fitfunc(p, x) - y
+        
+        p0 = scipy.c_[peak, position, 5]
+        p1, success = scipy.optimize.leastsq(errfunc, p0.copy()[0], args=(spectrum[index - m:p + index].wavelength, spectrum[index - m:p + index].flux))
+        
+        profiles.append(p1)
+        
+    if len(profiles) == 1:
+        return profiles[0]
     
-    return p1    
-    
+    return profiles
 
 
