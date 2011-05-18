077761533aadbb1d99cab409440e6b4455f6b1ef
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2011-05-18 22:15:41 +1000

    in the middle of adding a fitter

--- oned/__init__.py
+++ oned/__init__.py
@@ -1,2 +1,2 @@
 from base import onedspec
-from util import continuum, fitprofs, fit_arclines, normalise, cross_correlate
+from util import continuum, fitprofs, fit_arclines, normalise, cross_correlate, normalise2
